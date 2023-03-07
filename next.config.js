13f152dafea9e8f0b24e3c09a432baabc8921079
Sayeef Alam <salam@cldpartners.com>
2023-03-07 10:28:32 -0500

    Base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/shanto20-dev.github.io",
-  assetPrefix: "/shanto20-dev.github.io/",
+  basePath: "/shanto20-dev",
+  assetPrefix: "/shanto20-dev/",
   reactStrictMode: true,
   images: {
     domains: ["shanto20-dev.github.io"],
