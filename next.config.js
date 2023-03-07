537da25580aaf480a4458595d3f0a8a1bb14a5e8
Sayeef Alam <salam@cldpartners.com>
2023-03-07 10:46:01 -0500

    edit skills array to have personal website

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  basePath: "/personal-website",
-  assetPrefix: "/personal-website/",
+  basePath: isProd ? "/personal-website" : "",
+  assetPrefix: isProd ? "/personal-website/" : "",
   reactStrictMode: true,
   images: {
     domains: ["shanto20-dev.github.io"],
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,27 +4,59 @@ import SkillTile from "./SkillTile";
 
 const skillsArray = [
   { name: "React", imageSource: "/personal-website/images/skills/react.png" },
-  { name: "Next.js", imageSource: "/images/skills/nextjs.png" },
-  { name: "Redux", imageSource: "/images/skills/redux.png" },
-  { name: "JavaScript", imageSource: "/images/skills/javascript.png" },
-  { name: "Ruby", imageSource: "/images/skills/ruby.png" },
-  { name: "Rails", imageSource: "/images/skills/rails.png" },
-  { name: "Express.js", imageSource: "/images/skills/express.png" },
-  { name: "Node.js", imageSource: "/images/skills/nodejs.png" },
-  { name: "GraphQL", imageSource: "/images/skills/graphql.png" },
-  { name: "Postgres", imageSource: "/images/skills/postgresql.png" },
-  { name: "MySQL", imageSource: "/images/skills/mysql.png" },
-  { name: "MongoDB", imageSource: "/images/skills/mongodb.png" },
-  { name: "HTML5", imageSource: "/images/skills/html-5.png" },
-  { name: "CSS3", imageSource: "/images/skills/css3.png" },
-  { name: "jQuery", imageSource: "/images/skills/jquery.png" },
-  { name: "Heroku", imageSource: "/images/skills/heroku.png" },
-  { name: "Webpack", imageSource: "/images/skills/webpack.png" },
-  { name: "Git", imageSource: "/images/skills/git.png" },
-  { name: "Salesforce", imageSource: "/images/skills/react.png" },
-  { name: "Apex", imageSource: "/images/skills/salesforce.svg" },
-  { name: "LWC", imageSource: "/images/skills/salesforce.svg" },
-  ,
+  {
+    name: "Next.js",
+    imageSource: "/personal-website/images/skills/nextjs.png",
+  },
+  { name: "Redux", imageSource: "/personal-website/images/skills/redux.png" },
+  {
+    name: "JavaScript",
+    imageSource: "/personal-website/images/skills/javascript.png",
+  },
+  { name: "Ruby", imageSource: "/personal-website/images/skills/ruby.png" },
+  { name: "Rails", imageSource: "/personal-website/images/skills/rails.png" },
+  {
+    name: "Express.js",
+    imageSource: "/personal-website/images/skills/express.png",
+  },
+  {
+    name: "Node.js",
+    imageSource: "/personal-website/images/skills/nodejs.png",
+  },
+  {
+    name: "GraphQL",
+    imageSource: "/personal-website/images/skills/graphql.png",
+  },
+  {
+    name: "Postgres",
+    imageSource: "/personal-website/images/skills/postgresql.png",
+  },
+  { name: "MySQL", imageSource: "/personal-website/images/skills/mysql.png" },
+  {
+    name: "MongoDB",
+    imageSource: "/personal-website/images/skills/mongodb.png",
+  },
+  { name: "HTML5", imageSource: "/personal-website/images/skills/html-5.png" },
+  { name: "CSS3", imageSource: "/personal-website/images/skills/css3.png" },
+  { name: "jQuery", imageSource: "/personal-website/images/skills/jquery.png" },
+  { name: "Heroku", imageSource: "/personal-website/images/skills/heroku.png" },
+  {
+    name: "Webpack",
+    imageSource: "/personal-website/images/skills/webpack.png",
+  },
+  { name: "Git", imageSource: "/personal-website/images/skills/git.png" },
+  {
+    name: "Salesforce",
+    imageSource: "/personal-website/images/skills/salesforce.png",
+  },
+  {
+    name: "Apex",
+    imageSource: "/personal-website/images/skills/salesforce.svg",
+  },
+  {
+    name: "LWC",
+    imageSource: "/personal-website/images/skills/salesforce.svg",
+  },
 ];
 
 const Skills = ({ nightMode }: { nightMode: boolean }) => {
