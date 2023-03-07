8d7e6e3be3d843cce91b042c02e0777d52462169
Sayeef Alam <salam@cldpartners.com>
2023-03-07 10:23:19 -0500

    Try new base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/shanto20-dev",
-  assetPrefix: "/shanto20-dev/",
+  basePath: "/shanto20-dev.github.io",
+  assetPrefix: "/shanto20-dev.github.io/",
   reactStrictMode: true,
   images: {
     domains: ["shanto20-dev.github.io"],
