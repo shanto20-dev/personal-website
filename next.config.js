824e410b724027b476559df3042f71f9b542d4b7
Sayeef Alam <salam@cldpartners.com>
2023-03-07 10:20:56 -0500

    Added base path for github

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/shanto20-dev",
+  assetPrefix: "/shanto20-dev/",
   reactStrictMode: true,
   images: {
     domains: ["shanto20-dev.github.io"],
